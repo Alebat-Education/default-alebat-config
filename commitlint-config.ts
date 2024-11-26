0f3c0e2be2a35d5ed8a431140c29ebd91f38495d
Eduardo Lopez <eduardo.lopez@alebateducation.com>
2024-11-26 09:15:10 +0100

    chore(config): increase subject max length to 100 characters

--- commitlint-config.ts
+++ commitlint-config.ts
@@ -15,7 +15,7 @@ export const commitLint = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    "subject-max-length": [2, "always", 50],
+    "subject-max-length": [2, "always", 100],
     "subject-case": [2, "never"],
     "scope-empty": [2, "never"],
     "scope-case": [2, "never"],
